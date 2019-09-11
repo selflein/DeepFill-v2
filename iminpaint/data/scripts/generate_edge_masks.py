18ffa2fde49a765c923f45ffeddf092483466809
Sven <elflein.sven@googlemail.com>
2019-09-12 01:06:15 +0200

    Added greyscale to rgb conversion script, added multiprocessing to edge mask generation

--- iminpaint/data/scripts/generate_edge_masks.py
+++ iminpaint/data/scripts/generate_edge_masks.py
@@ -6,6 +6,9 @@ import cv2 as cv
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from functools import partial
+from skimage.io import imread
 
 # Based on
 # https://github.com/opencv/opencv/blob/master/samples/dnn/edge_detection.py
@@ -47,37 +50,43 @@ class CropLayer(object):
         return [inputs[0][:, :, self.ystart:self.yend, self.xstart:self.xend]]
 
 
+def chunks(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     cv.dnn_registerLayer('Crop', CropLayer)
 
     # Load the model.
     net = cv.dnn.readNetFromCaffe(args.prototxt, args.caffemodel)
-    img_files = list(Path(args.input).iterdir())
-    for img_path in tqdm(img_files, desc='Creating edge masks...'):
-        frame = cv.imread(str(img_path))
+    img_files = list(chunks(list(Path(args.input).iterdir()), 16))
+    for img_chunk in tqdm(img_files, desc='Creating edge masks...'):
+        with Pool(8) as reader_pool:
+            frames = reader_pool.map(imread, img_chunk)
 
-        height, width, _ = frame.shape
+        height, width, ch = frames[0].shape
 
-        inp = cv.dnn.blobFromImage(frame, scalefactor=1.0,
-                                   size=(width, height),
-                                   mean=(
-                                       104.00698793, 116.66876762,
-                                       122.67891434),
-                                   swapRB=False, crop=False)
+        inp = cv.dnn.blobFromImages(frames, scalefactor=1.0,
+                                    size=(width, height),
+                                    mean=(
+                                        104.00698793, 116.66876762,
+                                        122.67891434),
+                                    swapRB=False, crop=False)
         net.setInput(inp)
-
         out = net.forward()
-        hed = cv.resize(out[0, 0], (width, height))
-        hed = (hed > .6).astype(np.int8) * 255
+        heds = [(hed[0, :, :] > .6).astype(np.int8) * 255 for hed in out]
         # kernel = np.ones((3, 3), np.uint8)
         # img_erosion = cv.erode(hed, kernel, iterations=1)
 
         if args.visualize:
             fig, ax = plt.subplots(3, 1)
-            ax[0].imshow(frame)
-            ax[1].imshow(hed)
+            ax[0].imshow(frames[0])
+            ax[1].imshow(heds[0])
             # ax[2].imshow(img_erosion)
             plt.show()
 
-        cv.imwrite(os.path.join(args.output, img_path.name), hed)
+        out_paths = map(lambda p: os.path.join(args.output, p.name), img_chunk)
+        with Pool(8) as reader_pool:
+            reader_pool.starmap(cv.imwrite, zip(out_paths, heds))
+
---
+++ iminpaint/data/scripts/greyscale_to_rgb.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+import cv2 as cv
+from skimage import io
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Convert greyscale to RGB images '
+                                        'inplace.')
+    parser.add_argument('--in_path', required=True)
+    args = parser.parse_args()
+
+    in_path = Path(args.in_path)
+
+    img_paths = [p for p in in_path.iterdir()]
+
+    for img_path in tqdm(img_paths):
+        try:
+            im = io.imread(str(img_path))
+        except (ValueError, SyntaxError) as e:
+            img_path.unlink()
+            print(img_path)
+            continue
+
+        if len(im.shape) != 3:
+            rgb_img = cv.cvtColor(im, cv.COLOR_GRAY2BGR)
+            try:
+                io.imsave(img_path, rgb_img)
+            except ValueError:
+                img_path.unlink()
+                print(img_path)
+                continue
